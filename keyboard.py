5ac4fc0ce3b07f71e8580291848836b6eb5629a1
Brunomdd <brunnodiniiz@gmail.com>
2025-06-06 22:16:35 -0300

    Adiciona script de controle de teclado

---
+++ keyboard.py
@@ -0,0 +1,20 @@
+import keyboard
+while True:
+    if keyboard.is_pressed('['):
+        keyboard.write(' Iae Diego ta tranquilo hoje?')
+        keyboard.press_and_release('enter')
+
+    elif keyboard.is_pressed("`"):
+        keyboard.write('Samuel é o cara')
+        keyboard.press_and_release('enter')
+
+    elif keyboard.is_pressed('.'):
+        keyboard.write('saindo mermao')
+        keyboard.press_and_release('enter')
+        break
+
+
+
+
+
+
